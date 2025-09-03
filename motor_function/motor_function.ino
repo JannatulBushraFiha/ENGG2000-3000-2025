6cd1a038f9e49cf9b46c90850356a4d1bdc33d87
JannatulBushraFiha <bushrafiha111@gmail.com>
2025-09-03 14:07:13 +1000

    Added code for LEDs

--- motor_function/motor_function.ino
+++ motor_function/motor_function.ino
@@ -1,10 +1,15 @@
+#define motor_driver_in1 5 //motor spin "high"
+#define motor_driver_in2 6 //motor spin "low"
+#define motor_driver_pwm 9 //motor speed control
+
+// LED Pins
+#define RED_WARNING_LIGHT 10 // turns red when there is marine vehicles coming in
+#define RED_ALTITUDE_LIGHT 11
+#define NIGHT_LIGHTS 12
 
 
-#define motor_driver_in1 = 5; //motor spin "high"
-#define motor_driver_in2 = 6; //motor spin "low"
-#define motor_driver_pwm = 9; //motor speed control
-#define bridge_up = false;
-#define bridge_stop = true;
+bool bridge_up = false;
+bool bridge_halt= true;
 
 
 //pins for sensor here
@@ -16,12 +21,14 @@ void setup() {
   pinMode(motor_driver_in1, OUTPUT);
   pinMode(motor_driver_in2, OUTPUT);
   pinMode(motor_driver_pwm, OUTPUT);
-                                                            ```````````````````````````````````````````````````````
+  pinMode (RED_WARNING_LIGHT, OUTPUT);
+  pinMode (RED_ALTITUDE_LIGHT, OUTPUT);
+                                                           
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+blink_altitude_light();
 }
 
 void automatedSensor(){
@@ -36,6 +43,7 @@ void automatedSensor(){
 void bridge_open(){
     digitalWrite(motor_driver_in1, HIGH);
     digitalWrite(motor_driver_in2, LOW);
+    warning_light();
     bridge_up = true;
 }
 
@@ -43,11 +51,37 @@ void bridge_open(){
 void bridge_close(){
     digitalWrite(motor_driver_in1, LOW);
     digitalWrite(motor_driver_in2, HIGH);
+    bridge_up = false;\
+    warning_light();
 }
 
 //it is run when the timer ends, or for emeregency stop
 void stop(){
     digitalWrite(motor_driver_in1, HIGH);
     digitalWrite(motor_driver_in2, HIGH);
-    bridge_stop = true;
+    bridge_halt= true;
+    warning_light();
 }
+
+void warning_light() {
+  if (bridge_up || bridge_halt) {
+    if (incoming marine vehicles sensed) { 
+      digitalWrite(RED_WARNING_LIGHT, HIGH);
+    }
+    else {
+      digitalWrite(RED_WARNING_LIGHT, LOW);
+      }
+  }
+}
+
+void blink_altitude_light() {
+  digitalWrite(RED_ALTITUDE_LIGHT, HIGH);
+  delay(500);
+  digitalWrite(RED_ALTITUDE_LIGHT, LOW);
+  delay(500);
+}
+
+void night_light_state() {
+
+}
+

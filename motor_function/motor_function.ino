ee9d014f80c414d502dc11965aa416921e1a4bd6
Chinkhusel Batsuuri <khusleebatsuuri@gmail.com>
2025-09-17 12:30:09 +1000

    changes

--- motor_function/motor_function.ino
+++ motor_function/motor_function.ino
@@ -1,9 +1,12 @@
-#define motor_driver_in1 5 //motor spin "high"
-#define motor_driver_in2 6 //motor spin "low"
+//#include wifi.h
+
+
+#define motor_driver_in1 23 //motor spin "high"
+#define motor_driver_in2 22 //motor spin "low"
 #define motor_driver_pwm 9 //motor speed control
 
 // LED Pins
-#define RED_WARNING_LIGHT 10 // turns red when there is marine vehicles coming in
+#define RED_WARNING_LIGHT 4 // turns red when there is marine vehicles coming in
 #define RED_ALTITUDE_LIGHT 11
 #define NIGHT_LIGHTS 12
 
@@ -21,14 +24,22 @@ void setup() {
   pinMode(motor_driver_in1, OUTPUT);
   pinMode(motor_driver_in2, OUTPUT);
   pinMode(motor_driver_pwm, OUTPUT);
-  pinMode (RED_WARNING_LIGHT, OUTPUT);
-  pinMode (RED_ALTITUDE_LIGHT, OUTPUT);
+  pinMode(RED_WARNING_LIGHT, OUTPUT);
+  pinMode(RED_ALTITUDE_LIGHT, OUTPUT);
                                                            
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-blink_altitude_light();
+    bridge_open();
+    digitalWrite(RED_WARNING_LIGHT, HIGH);
+    delay(1000);
+    digitalWrite(RED_WARNING_LIGHT, LOW);
+    delay(1000);
+
+
+
+
 }
 
 void automatedSensor(){
@@ -43,8 +54,8 @@ void automatedSensor(){
 void bridge_open(){
     digitalWrite(motor_driver_in1, HIGH);
     digitalWrite(motor_driver_in2, LOW);
-    warning_light();
-    bridge_up = true;
+    //warning_light();
+    //bridge_up = true;
 }
 
 //for given time that the bridge will take to reach ground
@@ -52,7 +63,7 @@ void bridge_close(){
     digitalWrite(motor_driver_in1, LOW);
     digitalWrite(motor_driver_in2, HIGH);
     bridge_up = false;\
-    warning_light();
+   // warning_light();
 }
 
 //it is run when the timer ends, or for emeregency stop
@@ -60,7 +71,7 @@ void stop(){
     digitalWrite(motor_driver_in1, HIGH);
     digitalWrite(motor_driver_in2, HIGH);
     bridge_halt= true;
-    warning_light();
+    //warning_light();
 }
 
 void warning_light() {
@@ -72,7 +83,7 @@ void warning_light() {
       digitalWrite(RED_WARNING_LIGHT, LOW);
       }
   }
-}
+}/*/
 
 void blink_altitude_light() {
   digitalWrite(RED_ALTITUDE_LIGHT, HIGH);

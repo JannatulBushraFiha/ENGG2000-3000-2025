45d0ead229f784f18f31e7a784e0690e0754529c
Wendy <tangruran@gmail.com>
2025-11-03 14:03:48 +1100

    Update Marine state in state.h

--- Testing Folder/Testing__Trial_Four/IntegratedCodeEngg/state.h
+++ Testing Folder/Testing__Trial_Four/IntegratedCodeEngg/state.h
@@ -4,6 +4,7 @@
 
 enum SystemMode : uint8_t { MODE_AUTO, MODE_MANUAL };
 enum BridgeCmd  : uint8_t { CMD_IDLE, CMD_OPEN, CMD_CLOSE, CMD_STOP };
+enum MarineStatus: uint8_t {MARINE_DETECTED, MARINE_PASSING, MARINE_DEPARTED, MARINE_CLEAR }; 
 
 // Defined ONCE in your main .ino:
 extern SystemMode g_mode;
@@ -13,3 +14,8 @@ extern float g_distance_cm;
 extern volatile BridgeCmd  g_cmd_manual;  // set by UI handlers
 extern volatile BridgeCmd  g_cmd_auto;
 extern volatile bool g_emergency;   // <-- NEW
+
+//Marine Status 
+extern volatile MarineStatus g_marine_status; 
+// Distance in cm at which the system registers a "DETECTED" event.
+const float OPEN_ON_CM = 19.0;
